a6e0dfd606cd268e23726449fd7365aeedebbe4c
Minh Dang <d.baminh@gmail.com>
2020-10-26 18:31:39 +0700

    [ADD] Complete tutorial and lab for week 2

---
+++ src/com/minhdang/lab_tut02/BubbleSort1.java
@@ -0,0 +1,49 @@
+package com.minhdang.lab_tut02;
+
+import java.util.ArrayList;
+
+/**
+ * Standard, no early termination, bubble sort.
+ *
+ * @author jkcchan
+ */
+public class BubbleSort1 {
+  /**
+   * Sorts the input array.
+   *
+   * @param array Arraylist of integers.
+   */
+  public void sort(ArrayList<Integer> array) {
+
+    for (int i = 0; i < array.size(); i++) {
+      for (int j = 0; j < array.size() - 1; j++) {
+        // check if we need to swap
+        if (array.get(j) > array.get(j + 1)) {
+          Integer temp = array.get(j);
+          array.set(j, array.get(j + 1));
+          array.set(j + 1, temp);
+        }
+      }
+    }
+  } // end of sort()
+
+  public static void main(String[] args) {
+
+    // construct the sorter we will be using
+    BubbleSort1 sorter = new BubbleSort1();
+
+    // read in input
+    ArrayList<Integer> array = BubbleSortUtils.getInput();
+
+    long startTime = System.nanoTime();
+    // sort
+    sorter.sort(array);
+
+    long endTime = System.nanoTime();
+
+    double estimatedTime = ((double) (endTime - startTime) / Math.pow(10, 9));
+    System.out.println("time taken = " + estimatedTime + " sec");
+    // print out sorted array
+    // BubbleSortUtils.print(array);
+  } // end of main()
+} // end of class BubbleSort1
---
+++ src/com/minhdang/lab_tut02/tut02.md
@@ -0,0 +1,26 @@
+2.1.9
+a) lower
+b) equal
+c) lower since n! = (n - 1)! * n
+
+2.2.2
+a) true
+b) true
+c) false
+d) true
+
+2.3.1
+n - 1
+
+2.3.4
+a) compute sum of square
+b) multiplication
+c) n
+d) O(n)
+
+2.4.1
+n
+
+2.4.4
+a) Q( n - 1 ) + 2n - 1
+b) n - 1
