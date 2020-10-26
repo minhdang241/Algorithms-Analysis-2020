3d4796dfd0ebb05649281e880f9530a2eccd4276
Minh Dang <d.baminh@gmail.com>
2020-10-26 09:18:09 +0700

    [ADD] Finish tutorial and lab for week 1

---
+++ src/com/minhdang/lab_tut01/DoubleLinkedList.java
@@ -0,0 +1,344 @@
+package com.minhdang.lab_tut01;
+/**
+ * Double linked list that implements interface MyList.
+ *
+ * @author Jeffrey Chan
+ */
+public class DoubleLinkedList implements MyList {
+  /** Reference to head of list. */
+  protected Node mHead;
+  /** Reference to tail of list. */
+  protected Node mTail;
+
+  /** Length of list. */
+  protected int mLength;
+
+  public DoubleLinkedList() {
+    mHead = null;
+    mTail = null;
+    mLength = 0;
+  } // end of DoubleLinkedList()
+
+  /**
+   * Add a new value to the start of the list.
+   *
+   * @param newValue Value to add to list.
+   */
+  public void add(int newValue) {
+    Node newNode = new Node(newValue);
+    if (mHead == null) {
+      mHead = newNode;
+      mTail = mHead;
+
+    } else {
+      newNode.setNext(mHead);
+      mHead.setPrev(newNode);
+      mHead = newNode;
+    }
+
+    mLength++;
+  } // end of add()
+
+  /**
+   * Add value (and corresponding node) at position 'index'. Indices start at 0.
+   *
+   * @param index Position in list to add new value to.
+   * @param newValue Value to add to list.
+   * @throws IndexOutOfBoundsException In index are out of bounds.
+   */
+  public void add(int index, int newValue) throws IndexOutOfBoundsException {
+    if (index >= mLength || index < 0) {
+      System.out.println(index);
+      throw new IndexOutOfBoundsException("Supplied index is invalid.");
+    }
+
+    if (mHead == null || index == 0) {
+      add(newValue);
+    } else {
+      Node newNode = new Node(newValue);
+      Node currNode;
+
+      if (index < Math.ceil((double) mLength / 2)) {
+        currNode = mHead;
+        for (int i = 0; i < index; i++) {
+          currNode = currNode.getNext();
+        }
+
+      } else {
+        currNode = mTail;
+        for (int i = mLength - 1; i > index; i--) {
+          currNode = currNode.getPrev();
+        }
+      }
+
+      Node prevNode = currNode.getPrev();
+
+      currNode.setPrev(newNode);
+      newNode.setNext(currNode);
+      newNode.setPrev(prevNode);
+      prevNode.setNext(newNode);
+      mLength++;
+    }
+  } // end of add()
+
+  /**
+   * Returns the value stored in node at position 'index' of list.
+   *
+   * @param index Position in list to get new value for.
+   * @return Value of element at specified position in list.
+   * @throws IndexOutOfBoundsException In index are out of bounds.
+   */
+  public int get(int index) throws IndexOutOfBoundsException {
+    if (index >= mLength || index < 0) {
+      throw new IndexOutOfBoundsException("Supplied index is invalid.");
+    }
+
+    Node currNode;
+    if (index < Math.ceil(mLength / 2)) {
+      currNode = mHead;
+      for (int i = 0; i < index; ++i) {
+        currNode = currNode.getNext();
+      }
+    } else {
+      currNode = mTail;
+      for (int i = mLength - 1; i > index; --i) {
+        currNode = currNode.getPrev();
+      }
+    }
+
+    return currNode.getValue();
+  } // end of get()
+
+  /**
+   * Returns the value stored in node at position 'index' of list.
+   *
+   * @param value Value to search for.
+   * @return True if value is in list, otherwise false.
+   */
+  public boolean search(int value) {
+    Node currNode = mHead;
+    for (int i = 0; i < mLength; ++i) {
+      if (currNode.getValue() == value) {
+        return true;
+      }
+      currNode = currNode.getNext();
+    }
+
+    return false;
+  } // end of search()
+
+  /**
+   * Delete given value from list (delete first instance found).
+   *
+   * @param value Value to remove.
+   * @return True if deletion was successful, otherwise false.
+   */
+  public boolean remove(int value) {
+    boolean isRemoved = false;
+
+    if (mHead.getValue() == value) {
+      mHead = mHead.getNext();
+      if (mHead != null) mHead.setPrev(null);
+      else mTail = null;
+      isRemoved = true;
+
+    } else if (mTail.getValue() == value) {
+      mTail = mTail.getPrev();
+      mTail.setNext(null);
+      isRemoved = true;
+
+    } else {
+      Node tempNode = mHead;
+      while (tempNode != null) {
+        if (tempNode.getValue() == value) {
+          Node nextNode = tempNode.getNext();
+          Node prevNode = tempNode.getPrev();
+          prevNode.setNext(nextNode);
+          if (nextNode != null) {
+            nextNode.setPrev(prevNode);
+          } else {
+            mTail = prevNode;
+          }
+          isRemoved = true;
+          break;
+        } else {
+          tempNode = tempNode.getNext();
+        }
+      }
+    }
+    mLength--;
+
+    return isRemoved;
+  } // end of delete()
+
+  /**
+   * Delete value (and corresponding node) at position 'index'. Indices start at 0.
+   *
+   * @param index Position in list to get new value for.
+   * @param dummy Dummy variable, serves no use apart from distinguishing overloaded methods.
+   * @return Value of node that was deleted.
+   */
+  public int remove(int index, boolean dummy) throws IndexOutOfBoundsException {
+    if (index >= mLength || index < 0) {
+      throw new IndexOutOfBoundsException("Supplied index is invalid.");
+    }
+    int value;
+    if (index == 0) {
+      value = mHead.getValue();
+      mHead = mHead.getNext();
+      if (mHead != null) mHead.setPrev(null);
+      else mTail = null;
+
+    } else if (index == mLength - 1) {
+      value = mTail.getValue();
+      mTail = mTail.getPrev();
+      mTail.setNext(null);
+
+    } else {
+      Node currNode;
+      if (index < Math.ceil((double) mLength / 2)) {
+        currNode = mHead;
+        for (int i = 0; i < index; i++) {
+          currNode = currNode.getNext();
+        }
+
+      } else {
+        currNode = mTail;
+        for (int i = mLength - 1; i > index; i--) {
+          currNode = currNode.getPrev();
+        }
+      }
+      value = currNode.getValue();
+      Node nextNode = currNode.getNext();
+      Node prevNode = currNode.getPrev();
+      prevNode.setNext(nextNode);
+      nextNode.setPrev(prevNode);
+    }
+
+    mLength--;
+    return value;
+  } // end of delete()
+
+  /**
+   * Returns the minimum value in the list.
+   *
+   * @return Minimum value in the list.
+   * @throws IndexOutOfBoundsException In index are out of bounds.
+   */
+  public int min() throws IllegalStateException {
+    if (mLength == 0) {
+      throw new IllegalStateException("Min is not defined for an empty list.");
+    }
+
+    // traverse list, looking for the minimum valued element
+    int minValue = mHead.getValue();
+    Node currNode = mHead.getNext();
+
+    while (currNode != null) {
+      if (currNode.getValue() < minValue) {
+        minValue = currNode.getValue();
+      }
+      currNode = currNode.getNext();
+    }
+
+    return minValue;
+  } // end of min()
+
+  /**
+   * Returns the maximum value in the list.
+   *
+   * @return Maximum value in the list.
+   * @throws IndexOutOfBoundsException In index are out of bounds.
+   */
+  public int max() throws IndexOutOfBoundsException {
+    if (mLength == 0) {
+      throw new IllegalStateException("Min is not defined for an empty list.");
+    }
+
+    // traverse list, looking for the minimum valued element
+    int maxValue = mHead.getValue();
+    Node currNode = mHead.getNext();
+
+    while (currNode != null) {
+      if (currNode.getValue() > maxValue) {
+        maxValue = currNode.getValue();
+      }
+      currNode = currNode.getNext();
+    }
+
+    return maxValue;
+  } // end of max()
+
+  /** Print the list in head to tail. */
+  public void print() {
+    System.out.println(toString());
+  } // end of print()
+
+  /** Print the list from tail to head. */
+  public void reversePrint() {
+    Node currNode = mTail;
+
+    StringBuilder str = new StringBuilder();
+
+    while (currNode != null) {
+      str.append(currNode.getValue() + " ");
+      currNode = currNode.getPrev();
+    }
+
+    System.out.println(str.toString());
+  } // end of reversePrint()
+
+  /** @return String representation of the list. */
+  public String toString() {
+    Node currNode = mHead;
+
+    StringBuffer str = new StringBuffer();
+
+    while (currNode != null) {
+      str.append(currNode.getValue() + " ");
+      currNode = currNode.getNext();
+    }
+
+    return str.toString();
+  } // end of getString();
+
+  /** Node type, inner private class. */
+  private class Node {
+    /** Stored value of node. */
+    private int mValue;
+    /** Reference to next node. */
+    private Node mNext;
+    /** Reference to previous node. */
+    private Node mPrev;
+
+    public Node(int value) {
+      mValue = value;
+      mNext = null;
+      mPrev = null;
+    }
+
+    public int getValue() {
+      return mValue;
+    }
+
+    public Node getNext() {
+      return mNext;
+    }
+
+    public Node getPrev() {
+      return mPrev;
+    }
+
+    public void setValue(int value) {
+      mValue = value;
+    }
+
+    public void setNext(Node next) {
+      mNext = next;
+    }
+
+    public void setPrev(Node prev) {
+      mPrev = prev;
+    }
+  } // end of inner class Node
+} // end of class DoubleLinkedList
---
+++ src/com/minhdang/lab_tut01/tut01.md
@@ -0,0 +1,66 @@
+1.4.3a  
+Operation: push(a)       
+Stack: [a]
+
+Operation: push(b)       
+Stack: [a, b]
+
+Operation: pop      
+Stack: [a]
+
+Operation: push(c)       
+Stack: [a, c]
+
+Operation: push(d)       
+Stack: [a, c, d]
+
+Operation: pop       
+Stack: [a, c]
+
+1.4.3b
+Operation: enqueue(a)       
+Stack: [a]
+
+Operation: enqueue(b)       
+Stack: [a, b]
+
+Operation: dequeue      
+Stack: [b]
+
+Operation: enqueue(c)       
+Stack: [b, c]
+
+Operation: enqueue(d)       
+Stack: [b, c, d]
+
+Operation: dequeue       
+Stack: [c, d]
+
+1.4.4a 
+a. All the entries, except diagonal entries, in the matrix must have value of 1.
+b. The diagonal entries must have value of 1;
+c. The adjacent matrix has an all-zero row. 
+
+1.4.4b
+a. Each list contains all the vertex
+b. The list contains the vertex define the list.
+c. Having an empty list.
+
+1.4.9
+a. priority queue 
+b. queue
+c. stacks
+
+1.4.2
+a. lists represented as arrays: => we can use binary search
+b. lists represented as linked lists: there is not much we can do in this case.
+
+1.4.10
+Algorithm:
+   
+```
+Check equal size
+    if Yes,
+       sorting both word, then compare 
+    if No return false
+```       
